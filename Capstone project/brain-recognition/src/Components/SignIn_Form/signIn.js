89380dfbf9edee2a807c65147177d5d3084eaec2
Ben <benabi.officials@gmail.com>
2024-02-29 05:29:29 -0800

    fixed htmlFor instead of for

--- Capstone project/brain-recognition/src/Components/SignIn_Form/signIn.js
+++ Capstone project/brain-recognition/src/Components/SignIn_Form/signIn.js
@@ -7,16 +7,16 @@ const SignIn = ({ onRouteChange }) => {
         <div >
             <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
-                    <form className="measure">
+                    <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f1 fw6 ph0 mh0">Sign In</legend>
                         <div className="mt3">
-                            <label className="db fw6 lh-copy f6">Email</label>
+                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                             <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" autoComplete="email" />
                         </div>
                         <div className="mv3">
-                            <label className="db fw6 lh-copy f6" >Password</label>
-                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" autoComplete="name"/>
+                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
                         </div>
                         </fieldset>
                         <div className="">
@@ -29,7 +29,7 @@ const SignIn = ({ onRouteChange }) => {
                         <div className="lh-copy mt3">
                         <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
                         </div>
-                    </form>
+                    </div>
                 </main>
             </article>
         </div>
