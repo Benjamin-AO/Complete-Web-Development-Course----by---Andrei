9eaea77c6cd9b5c041867299edd7e512ec839358
Ben <benabi.officials@gmail.com>
2024-03-07 01:24:18 -0800

    postMan lesson 1 completed

--- node_Tutorial/server.js
+++ node_Tutorial/server.js
@@ -2,8 +2,20 @@ const express = require('express');
 const app = express()
 
 
+app.use(express.urlencoded({extended:false}));  //this is a middle-ware
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send("hello");
-})
+    res.send("getting to root");
+});
+
+app.get('/profile', (req, res) => {
+    res.send("getting profile");
+});
+
+app.post('/profile', (req, res) => {
+    console.log(req.body)
+    res.send('success');
+});
 
 app.listen(3000);
