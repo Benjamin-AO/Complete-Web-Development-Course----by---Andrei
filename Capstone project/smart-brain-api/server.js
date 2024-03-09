9ac38baefc2ec54f30939502d55098a5776963ed
Ben <benabi.officials@gmail.com>
2024-03-09 05:25:09 -0800

    started api server

--- Capstone project/smart-brain-api/server.js
+++ Capstone project/smart-brain-api/server.js
@@ -2,8 +2,56 @@ const express = require('express');
 
 const app = express();
 
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: "cookies",
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('this is working')
+    res.json(database.users);
+})
+
+app.post('/signin', (req,res) =>{
+    if (req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })  
+    res.json(database.users[database.users.length-1]); // we are passing the index of the last database user object to json
 })
 
 app.listen(3000, () => {
